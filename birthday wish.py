1a369e9f958dcfaf014574f2478d4accf52f9497
Muni Balaji . A <100946453+MUNIBALAJI-AMBITI@users.noreply.github.com>
2022-08-16 23:08:31 +0530

    Add files via upload

---
+++ birthday wish.py
@@ -0,0 +1,256 @@
+from __future__ import print_function
+import time
+import sys
+
+
+try:
+    import winsound
+except ImportError:
+    import os
+
+name = "Enter name" #Enter the name here
+
+print (" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print ("	HAPPY BIRTHDAY ****Name**** A SMALL ATTEMPT TO MAKE YOUR BIRTHDAY SPECIAL :)")
+print ("    ======================================================================")
+print (" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+try:
+	winsound.Beep(264, 250)
+	sys.stdout.write('Ha')
+	time.sleep(500/2000.0)
+	sys.stdout.write('ppy ')
+	winsound.Beep(264, 250)
+	time.sleep(250/2000.0)
+	sys.stdout.write('birth')
+	winsound.Beep(297, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	winsound.Beep(264, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('to ')
+	winsound.Beep(352, 1000)
+	time.sleep(250/2000.0)
+	print('you')
+	winsound.Beep(330, 2000)
+	time.sleep(500/2000.0)
+
+	sys.stdout.write('Ha')
+	winsound.Beep(264, 250)
+	time.sleep(500/2000.0)
+	sys.stdout.write('ppy ')
+	winsound.Beep(264, 250)
+	time.sleep(250/2000.0)
+	sys.stdout.write('birth')
+	winsound.Beep(297, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	winsound.Beep(264, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('to ')
+	winsound.Beep(396, 1000)
+	time.sleep(250/2000.0)
+	print('you')
+	winsound.Beep(352, 2000)
+	time.sleep(500/2000.0)
+
+	sys.stdout.write('Ha')
+	winsound.Beep(264, 250)
+	time.sleep(250/2000.0)
+	sys.stdout.write('ppy ')
+	winsound.Beep(264, 500)
+	time.sleep(250/1000.0)
+	sys.stdout.write('birth')
+	winsound.Beep(440, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	winsound.Beep(352, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('dear ')
+	winsound.Beep(330, 1000)
+	print(name)
+	time.sleep(250/2000.0)
+	winsound.Beep(297, 1000)
+
+	winsound.Beep(440, 1000)
+	time.sleep(250/2000.0)
+
+	time.sleep(500/2000.0)
+	sys.stdout.write('Ha')
+	winsound.Beep(466, 250)
+	time.sleep(500/2000.0)
+	sys.stdout.write('ppy ')
+	winsound.Beep(466, 250)
+	time.sleep(250/2000.0)
+	sys.stdout.write('birth')
+	winsound.Beep(440, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	winsound.Beep(352, 1000)
+	time.sleep(250/2000.0)
+	sys.stdout.write('to ')
+	winsound.Beep(396, 1000)
+	time.sleep(250/2000.0)
+	print('you')
+	winsound.Beep(352, 2000)
+	time.sleep(250/2000.0)
+except:
+	os.system('beep -f 264 -l 250')
+	sys.stdout.write('Ha')
+	sys.stdout.flush()
+	time.sleep(500/2000.0)
+	sys.stdout.write('ppy ')
+	sys.stdout.flush()
+	os.system('beep -f 264 -l 250')
+	time.sleep(250/2000.0)
+	sys.stdout.write('birth')
+	sys.stdout.flush()
+	os.system('beep -f 297 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	sys.stdout.flush()
+	os.system('beep -f 264 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('to ')
+	sys.stdout.flush()
+	os.system('beep -f 352 -l 1000')
+	time.sleep(250/2000.0)
+	print ('you')
+	os.system('beep -f 330 -l 2000')
+	time.sleep(500/2000.0)
+
+	sys.stdout.write('Ha')
+	sys.stdout.flush()
+	os.system('beep -f 264 -l 250')
+	time.sleep(500/2000.0)
+	sys.stdout.write('ppy ')
+	sys.stdout.flush()
+	os.system('beep -f 264 -l 250')
+	time.sleep(250/2000.0)
+	sys.stdout.write('birth')
+	sys.stdout.flush()
+	os.system('beep -f 297 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	sys.stdout.flush()
+	os.system('beep -f 264 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('to ')
+	sys.stdout.flush()
+	os.system('beep -f 396 -l 1000')
+	time.sleep(250/2000.0)
+	print ('you')
+	os.system('beep -f 352 -l 2000')
+	time.sleep(500/2000.0)
+
+	sys.stdout.write('Ha')
+	sys.stdout.flush()
+	os.system('beep -f 264 -l 250')
+	time.sleep(250/2000.0)
+	sys.stdout.write('ppy ')
+	sys.stdout.flush()
+	os.system('beep -f 264 -l 500')
+	time.sleep(250/1000.0)
+	sys.stdout.write('birth')
+	sys.stdout.flush()
+	os.system('beep -f 440 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	sys.stdout.flush()
+	os.system('beep -f 352 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('dear ')
+	sys.stdout.flush()
+	os.system('beep -f 330 -l 1000')
+	print (name)
+	time.sleep(250/2000.0)
+	os.system('beep -f 297 -l 1000')
+
+	os.system('beep -f 440 -l 1000')
+	time.sleep(250/2000.0)
+
+	time.sleep(500/2000.0)
+	sys.stdout.write('Ha')
+	sys.stdout.flush()
+	os.system('beep -f 466 -l 250')
+	time.sleep(500/2000.0)
+	sys.stdout.write('ppy ')
+	sys.stdout.flush()
+	os.system('beep -f 466 -l 250')
+	time.sleep(250/2000.0)
+	sys.stdout.write('birth')
+	sys.stdout.flush()
+	os.system('beep -f 440 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('day ')
+	sys.stdout.flush()
+	os.system('beep -f 352 -l 1000')
+	time.sleep(250/2000.0)
+	sys.stdout.write('to ')
+	sys.stdout.flush()
+	os.system('beep -f 396 -l 1000')
+	time.sleep(250/2000.0)
+	print ('you')
+	os.system('beep -f 352 -l 2000')
+	time.sleep(250/2000.0)
+
+
+print ('HAPPY BIRTHDAY ' + name + ' FROM:****Name**** ;) HAVE A BLAST AND STAY SAFE')
+
+print("""                                                                                                                                                                                                                              
+                                                                                                                                                                                                                        
+HHHHHHHHH     HHHHHHHHH               AAA               PPPPPPPPPPPPPPPPP   PPPPPPPPPPPPPPPPP   YYYYYYY       YYYYYYY                                                                                                   
+H:::::::H     H:::::::H              A:::A              P::::::::::::::::P  P::::::::::::::::P  Y:::::Y       Y:::::Y                                                                                                   
+H:::::::H     H:::::::H             A:::::A             P::::::PPPPPP:::::P P::::::PPPPPP:::::P Y:::::Y       Y:::::Y                                                                                                   
+HH::::::H     H::::::HH            A:::::::A            PP:::::P     P:::::PPP:::::P     P:::::PY::::::Y     Y::::::Y                                                                                                   
+  H:::::H     H:::::H             A:::::::::A             P::::P     P:::::P  P::::P     P:::::PYYY:::::Y   Y:::::YYY                                                                                                   
+  H:::::H     H:::::H            A:::::A:::::A            P::::P     P:::::P  P::::P     P:::::P   Y:::::Y Y:::::Y                                                                                                      
+  H::::::HHHHH::::::H           A:::::A A:::::A           P::::PPPPPP:::::P   P::::PPPPPP:::::P     Y:::::Y:::::Y                                                                                                       
+  H:::::::::::::::::H          A:::::A   A:::::A          P:::::::::::::PP    P:::::::::::::PP       Y:::::::::Y                                                                                                        
+  H:::::::::::::::::H         A:::::A     A:::::A         P::::PPPPPPPPP      P::::PPPPPPPPP          Y:::::::Y                                                                                                         
+  H::::::HHHHH::::::H        A:::::AAAAAAAAA:::::A        P::::P              P::::P                   Y:::::Y                                                                                                          
+  H:::::H     H:::::H       A:::::::::::::::::::::A       P::::P              P::::P                   Y:::::Y                                                                                                          
+  H:::::H     H:::::H      A:::::AAAAAAAAAAAAA:::::A      P::::P              P::::P                   Y:::::Y                                                                                                          
+HH::::::H     H::::::HH   A:::::A             A:::::A   PP::::::PP          PP::::::PP                 Y:::::Y                                                                                                          
+H:::::::H     H:::::::H  A:::::A               A:::::A  P::::::::P          P::::::::P              YYYY:::::YYYY                                                                                                       
+H:::::::H     H:::::::H A:::::A                 A:::::A P::::::::P          P::::::::P              Y:::::::::::Y                                                                                                       
+HHHHHHHHH     HHHHHHHHHAAAAAAA                   AAAAAAAPPPPPPPPPP          PPPPPPPPPP              YYYYYYYYYYYYY                                                                                                       
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+BBBBBBBBBBBBBBBBB   IIIIIIIIIIRRRRRRRRRRRRRRRRR   TTTTTTTTTTTTTTTTTTTTTTTHHHHHHHHH     HHHHHHHHHDDDDDDDDDDDDD                       AAA               YYYYYYY       YYYYYYY                                             
+B::::::::::::::::B  I::::::::IR::::::::::::::::R  T:::::::::::::::::::::TH:::::::H     H:::::::HD::::::::::::DDD                   A:::A              Y:::::Y       Y:::::Y                                             
+B::::::BBBBBB:::::B I::::::::IR::::::RRRRRR:::::R T:::::::::::::::::::::TH:::::::H     H:::::::HD:::::::::::::::DD                A:::::A             Y:::::Y       Y:::::Y                                             
+BB:::::B     B:::::BII::::::IIRR:::::R     R:::::RT:::::TT:::::::TT:::::THH::::::H     H::::::HHDDD:::::DDDDD:::::D              A:::::::A            Y::::::Y     Y::::::Y                                             
+  B::::B     B:::::B  I::::I    R::::R     R:::::RTTTTTT  T:::::T  TTTTTT  H:::::H     H:::::H    D:::::D    D:::::D            A:::::::::A           YYY:::::Y   Y:::::YYY                                             
+  B::::B     B:::::B  I::::I    R::::R     R:::::R        T:::::T          H:::::H     H:::::H    D:::::D     D:::::D          A:::::A:::::A             Y:::::Y Y:::::Y                                                
+  B::::BBBBBB:::::B   I::::I    R::::RRRRRR:::::R         T:::::T          H::::::HHHHH::::::H    D:::::D     D:::::D         A:::::A A:::::A             Y:::::Y:::::Y                                                 
+  B:::::::::::::BB    I::::I    R:::::::::::::RR          T:::::T          H:::::::::::::::::H    D:::::D     D:::::D        A:::::A   A:::::A             Y:::::::::Y                                                  
+  B::::BBBBBB:::::B   I::::I    R::::RRRRRR:::::R         T:::::T          H:::::::::::::::::H    D:::::D     D:::::D       A:::::A     A:::::A             Y:::::::Y                                                   
+  B::::B     B:::::B  I::::I    R::::R     R:::::R        T:::::T          H::::::HHHHH::::::H    D:::::D     D:::::D      A:::::AAAAAAAAA:::::A             Y:::::Y                                                    
+  B::::B     B:::::B  I::::I    R::::R     R:::::R        T:::::T          H:::::H     H:::::H    D:::::D     D:::::D     A:::::::::::::::::::::A            Y:::::Y                                                    
+  B::::B     B:::::B  I::::I    R::::R     R:::::R        T:::::T          H:::::H     H:::::H    D:::::D    D:::::D     A:::::AAAAAAAAAAAAA:::::A           Y:::::Y                                                    
+BB:::::BBBBBB::::::BII::::::IIRR:::::R     R:::::R      TT:::::::TT      HH::::::H     H::::::HHDDD:::::DDDDD:::::D     A:::::A             A:::::A          Y:::::Y                                                    
+B:::::::::::::::::B I::::::::IR::::::R     R:::::R      T:::::::::T      H:::::::H     H:::::::HD:::::::::::::::DD     A:::::A               A:::::A      YYYY:::::YYYY                                                 
+B::::::::::::::::B  I::::::::IR::::::R     R:::::R      T:::::::::T      H:::::::H     H:::::::HD::::::::::::DDD      A:::::A                 A:::::A     Y:::::::::::Y                                                 
+BBBBBBBBBBBBBBBBB   IIIIIIIIIIRRRRRRRR     RRRRRRR      TTTTTTTTTTT      HHHHHHHHH     HHHHHHHHHDDDDDDDDDDDDD        AAAAAAA                   AAAAAAA    YYYYYYYYYYYYY                                                 
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                          
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+                                                                                                                  
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           """)
